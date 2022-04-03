ecf87698178c091c83fbbab80099e72761647c0c
louie_gomez <kinggomez027.lg@gmail.com>
2022-04-03 17:31:38 -0400

    commit tree ee953f9b5eedd615417f5f4f7c61548d97e18401

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = 31;
+const total = batteryBatches.reduce((acc, item) => {
+    return acc + item
+}, totalBatteries)
